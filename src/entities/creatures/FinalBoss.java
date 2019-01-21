2a55873e6619aa0d090caa5f1849d020fe9cdbab
Fergus Reid <rincewind57@hotmail.com>
2019-01-21 14:32:41 +0000

    Gave final boss all loadable skills.

--- src/entities/creatures/FinalBoss.java
+++ src/entities/creatures/FinalBoss.java
@@ -2,6 +2,8 @@ package entities.creatures;
 
 import control.Map;
 import enums.Faction;
+import enums.LoadableSkill;
+import mapgeneration.data.providers.SkillProvider;
 import skills.Skill;
 import ui.mainwindow.Messages;
 
@@ -13,6 +15,13 @@ public class FinalBoss extends Monster
     {
         super('P', inRow, inColumn, inMap, inMessages, Faction.FOE, 144, "Lord Picaro", 1, 432*8, 300, 317, 10, 5, 0, 1, 337,
                 177, 177, 177, 177, 317, 317, new ArrayList<Skill>(), level, level);
+
+        // Give final boss all loadable skills.
+        SkillProvider skillProvider = new SkillProvider();
+        for(LoadableSkill loadableSkill: LoadableSkill.values())
+        {
+            getSkills().add(skillProvider.get(loadableSkill));
+        }
     }
 
     public void onDeath()
